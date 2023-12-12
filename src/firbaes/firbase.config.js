79efd8835b6e687acb4c2605fdbeffedcc603964
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-12 15:07:26 +0600

    firbase-commit

---
+++ src/firbaes/firbase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfbLqxyohnt-BFeq8yDvlBQ32GCzT4hzQ",
+  authDomain: "coffee-stors-clid-side.firebaseapp.com",
+  projectId: "coffee-stors-clid-side",
+  storageBucket: "coffee-stors-clid-side.appspot.com",
+  messagingSenderId: "671459728539",
+  appId: "1:671459728539:web:360ed9cc1d61be88f43a3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
